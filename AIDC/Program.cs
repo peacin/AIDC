38ef5f7f485da2bff72e3529db76037be067cd4c
peacin <88489967+peacin@users.noreply.github.com>
2021-08-05 14:52:46 +0300

    files

---
+++ AIDC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AIDC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.GetProcessesByName("AnyDesk")[0].Kill(); //Kill the process of anydesk
+
+            File.Delete("C:\\ProgramData\\AnyDesk\\system.conf"); //Delete config files.
+            File.Delete("C:\\ProgramData\\AnyDesk\\service.conf");
+
+            Console.WriteLine("Start AnyDesk");
+            Thread.Sleep(5000);
+            Environment.Exit(0); //Exists the program after 5 seconds.
+
+        }
+    }
+}
